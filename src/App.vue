3f8a0e0a764bd4a99e5b77348027d86bd07f8d3a
s31237737 <s1237737@nate.com>
2025-03-02 14:43:11 +0900

    초기작업

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <template>
   <v-app :class="displaySize">
-    <Header />
-    <div>
-      <p v-if="!isMobile">
-        💻 PC 화면입니다!
-      </p>
-      <p v-else>
-        📱 모바일 화면입니다!
-      </p>
-    </div>
     <router-view />
-    <Footer />
   </v-app>
 </template>
 
@@ -21,7 +11,7 @@ import { useDisplay } from 'vuetify';
 
 const { mobile } = useDisplay();
 provide('isMobile', mobile);
-const isMobile = computed(() => mobile.value);
+
 const displaySize = computed(() => {
   return mobile.value ? 'mobile' : 'desktop';
 });
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,21 @@
 <template>
+  <Header />
   <v-main>
+    <div>
+      <p v-if="!isMobile">
+        💻 PC 화면입니다!
+      </p>
+      <p v-else>
+        📱 모바일 화면입니다!
+      </p>
+    </div>
     <router-view />
   </v-main>
+  <Footer />
 </template>
 
 
 <script setup>
+import { inject } from 'vue';
+const isMobile = inject('isMobile');
 </script>
