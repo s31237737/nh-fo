c35dc4af5bb57aef41efe55824000f19450caf03
s31237737 <s1237737@nate.com>
2025-03-04 11:22:42 +0900

    초기작업

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,13 +14,44 @@ import { createVuetify } from 'vuetify'
 import { aliases } from "vuetify/lib/iconsets/mdi-svg";
 import { custom } from "@/utils/vuetifyIcons";
 
+const nhFo = {
+  dark: false,
+  colors: {
+    background: '#F3F4F9',
+    surface: '#FFFFFF',
+    primary: '#1E1E1E',
+    secondary: '#48A9A6',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+  variables: {
+    'border-color': '#000000',
+    'border-opacity': 0.12,
+    'high-emphasis-opacity': 0.87,
+    'medium-emphasis-opacity': 0.60,
+    'disabled-opacity': 0.38,
+    'idle-opacity': 0.04,
+    'hover-opacity': 0.04,
+    'focus-opacity': 0.12,
+    'selected-opacity': 0.08,
+    'activated-opacity': 0.12,
+    'pressed-opacity': 0.12,
+    'dragged-opacity': 0.08,
+    'theme-kbd': '#212529',
+    'theme-on-kbd': '#FFFFFF',
+    'theme-code': '#F5F5F5',
+    'theme-on-code': '#000000',
+  }
+}
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'light',
-    light: {
-      colors: {
-      },
+    defaultTheme: 'nhFo',
+     themes: {
+      nhFo,
     },
   },
   icons: {
