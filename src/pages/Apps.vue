0cdf86ee980daae83784c49ce25a47a20cffb9dd
LAPTOP-20P7R6SQ\joo2 <ehrqor1641@gmail.com>
2025-03-06 15:09:42 +0900

    apps/card 작업중

--- src/pages/Apps.vue
+++ src/pages/Apps.vue
@@ -24,12 +24,10 @@
     class="inner"
   > 
     <v-row
-      no-gutters
       class="pt-4 pb-4"
     >
+      <!-- type 1 -->
       <v-col
-        v-for="n in 3"
-        :key="n"
         cols="12"
         xs="4"
         sm="6"
@@ -39,15 +37,124 @@
           class="mx-auto"
           variant="flat"
         >
-          <v-img
-            :width="60"
-            :height="60"
-            aspect-ratio="16/9"
-            cover
-            src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-          />
-          <v-card-title>공통총무알리미</v-card-title>
-          <v-card-text>
+          <div class="d-flex justify-space-between">
+            <div class="thumnail">
+              <v-img
+                :width="60"
+                :height="60"
+                aspect-ratio="16/9"
+                cover
+                src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+              />
+            </div>
+            <!-- flag: 최대 3개 노출 -->
+            <div class="flags">
+              <v-chip
+                class="flag"
+                color="secondary"
+                variant="tonal"
+                density="comfortable"
+              >
+                blue
+              </v-chip>
+              <v-chip
+                class="flag"
+                color="purple"
+                variant="tonal"
+                density="comfortable"
+              >
+                purple
+              </v-chip>
+              <v-chip
+                class="flag"
+                color="pink"
+                variant="tonal"
+                density="comfortable"
+              >
+                pink
+              </v-chip>
+
+              <!-- status: 사용신청 완료일 경우 -->
+              <v-chip
+                class="flag"
+                color="primary"
+                density="comfortable"
+              >
+                black
+              </v-chip>
+            </div>
+          </div>
+
+          <v-card-title class="pt-8">
+            공통총무알리미
+          </v-card-title>
+          <v-card-text class="pt-3">
+            농협 및 축협 이용 시 필수 예약 과정으로 편리한 예약 신청 및 관리를 도와주는 편리한 농협 앱
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <!-- type 2 -->
+      <v-col
+        cols="12"
+        xs="4"
+        sm="6"
+        md="4"
+      >
+        <v-card
+          class="mx-auto"
+          variant="flat"
+        >
+          <div class="d-flex justify-space-between">
+            <v-avatar
+              rounded="0"
+              size="60"
+            >
+              <v-img src="https://cdn.vuetifyjs.com/images/cards/halcyon.png" />
+            </v-avatar>
+            <!-- flag: 최대 3개 노출 -->
+            <div class="flags">
+              <v-chip
+                class="flag"
+                color="secondary"
+                variant="tonal"
+                density="comfortable"
+              >
+                blue
+              </v-chip>
+              <v-chip
+                class="flag"
+                color="purple"
+                variant="tonal"
+                density="comfortable"
+              >
+                purple
+              </v-chip>
+              <v-chip
+                class="flag"
+                color="pink"
+                variant="tonal"
+                density="comfortable"
+              >
+                pink
+              </v-chip>
+
+              <!-- status: 사용신청 완료일 경우 -->
+              <v-chip
+                class="flag"
+                color="primary"
+                density="comfortable"
+              >
+                black
+              </v-chip>
+            </div>
+          </div>
+
+
+          <v-card-title class="pt-8">
+            공통총무알리미
+          </v-card-title>
+          <v-card-text class="pt-3">
             농협 및 축협 이용 시 필수 예약 과정으로 편리한 예약 신청 및 관리를 도와주는 편리한 농협 앱
           </v-card-text>
         </v-card>
