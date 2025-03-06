2d06e5e455b96898e6ec41a37aec33daf6ef33b6
s31237737 <s1237737@nate.com>
2025-03-06 09:12:54 +0900

    헤더작업중

--- src/components/Alarm.vue
+++ src/components/Alarm.vue
@@ -61,85 +61,108 @@
           </v-card-text>
         </template>
         <template v-else>
-          <!-- 관리자 알림 -->
-          <v-chip-group
-            v-model="activeTab"
-            variant="outlined"
-            class="popover-tab"
-            selected-class="selected"
-            mandatory
-          >
-            <v-chip
-              size="small"
-              value="store"
-            >
-              업스토어 알림
-            </v-chip>
-            <v-chip
-              size="small"
-              value="admin"
-            >
-              관리자 알림
-            </v-chip>
-          </v-chip-group>
-          <v-card-text>
-            <v-window
-              v-model="activeTab"
-            >
-              <v-window-item value="store">
-                <v-list v-if="notifications.length">
-                  <v-list-item
-                    v-for="(item, index) in notifications"
-                    :key="index"
-                  >
-                    <v-list-item-title :class="{'dot':item.isNew}">
-                      {{ item.title }}
-                    </v-list-item-title>
-                    <v-list-item-subtitle>{{ item.date }}</v-list-item-subtitle>
-                  </v-list-item>
-                </v-list>
-                <v-empty-state
-                  v-else
-                  text="알림이 없습니다."
-                  icon="custom:warning"
-                  size="40"
-                />
-              </v-window-item>
-
-              <v-window-item value="admin">
-                <v-list v-if="notifications.length">
-                  <v-list-item
-                    v-for="(item, index) in notifications"
-                    :key="index"
-                  >
-                    <v-list-item-title :class="{'dot':item.isNew}">
-                      {{ item.title }}
-                    </v-list-item-title>
-                    <v-list-item-subtitle>{{ item.date }}</v-list-item-subtitle>
-                  </v-list-item>
-                </v-list>
-                <v-empty-state
-                  v-else
-                  text="알림이 없습니다."
-                  icon="custom:warning"
-                  size="40"
-                />
-              </v-window-item>
-            </v-window>
+          <!-- 일반 알림 -->
+          <v-card-text v-if="!isAdmin">
+            <v-list v-if="notifications.length">
+              <v-list-item
+                v-for="(item, index) in notifications"
+                :key="index"
+              >
+                <v-list-item-title :class="{'dot':item.isNew}">
+                  {{ item.title }}
+                </v-list-item-title>
+                <v-list-item-subtitle>{{ item.date }}</v-list-item-subtitle>
+              </v-list-item>
+            </v-list>
+            <v-empty-state
+              v-else
+              text="알림이 없습니다."
+              icon="custom:warning"
+              size="40"
+            />
           </v-card-text>
+          <!-- // 일반 알림 -->
+          <!-- 관리자 알림 -->
+          <template v-else>
+            <v-chip-group
+              v-model="activeTab"
+              variant="outlined"
+              class="popover-tab"
+              selected-class="selected"
+              mandatory
+            >
+              <v-chip
+                size="small"
+                value="store"
+              >
+                업스토어 알림
+              </v-chip>
+              <v-chip
+                size="small"
+                value="admin"
+              >
+                관리자 알림
+              </v-chip>
+            </v-chip-group>
+            <v-card-text>
+              <v-window
+                v-model="activeTab"
+              >
+                <v-window-item value="store">
+                  <v-list v-if="notifications.length">
+                    <v-list-item
+                      v-for="(item, index) in notifications"
+                      :key="index"
+                    >
+                      <v-list-item-title :class="{'dot':item.isNew}">
+                        {{ item.title }}
+                      </v-list-item-title>
+                      <v-list-item-subtitle>{{ item.date }}</v-list-item-subtitle>
+                    </v-list-item>
+                  </v-list>
+                  <v-empty-state
+                    v-else
+                    text="알림이 없습니다."
+                    icon="custom:warning"
+                    size="40"
+                  />
+                </v-window-item>
+
+                <v-window-item value="admin">
+                  <v-list v-if="notifications.length">
+                    <v-list-item
+                      v-for="(item, index) in notifications"
+                      :key="index"
+                    >
+                      <v-list-item-title :class="{'dot':item.isNew}">
+                        {{ item.title }}
+                      </v-list-item-title>
+                      <v-list-item-subtitle>{{ item.date }}</v-list-item-subtitle>
+                    </v-list-item>
+                  </v-list>
+                  <v-empty-state
+                    v-else
+                    text="알림이 없습니다."
+                    icon="custom:warning"
+                    size="40"
+                  />
+                </v-window-item>
+              </v-window>
+            </v-card-text>
+          </template>
         <!-- // 관리자 알림 -->
         </template>
       </v-card>
     </v-menu>
   </v-btn>
 </template>
-<script setup>
-import { ref } from 'vue';
+<script setup>import { ref } from 'vue';
 const hasNotification = ref(true); // 알림이 있는지 여부
 const isSetting= ref(false); // 설정 버튼 클릭시
+const isAdmin= ref(true); // 관리자 권한이 있는지 여부
 
 const activeTab = ref('store'); // 현재 활성화된 탭
-const emailSwitch = ref(true); // 이메일 송신 스위치
+const emailSwitch = ref(false); // 이메일 송신 스위치
 
 const notifications = ref([
   {
@@ -187,6 +210,7 @@ const notifications = ref([
     date: '2025-09-30 09:38',
   },
 ]);
+
 // 알림 설정 토글
 const toggleSetting = () => {
   isSetting.value = !isSetting.value;
