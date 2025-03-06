b0797499a8cba14d33a1c90f7d16cc1a9aed33cf
LAPTOP-20P7R6SQ\joo2 <ehrqor1641@gmail.com>
2025-03-06 18:22:44 +0900

    apps 작업중

--- src/pages/Apps.vue
+++ src/pages/Apps.vue
@@ -23,6 +23,10 @@
   <v-container
     class="inner"
   >
+    <p class="title-2 pb-10">
+      앱 전체 리스트
+    </p>
+    <!-- 앱 목록  -->
     <v-row
       class="appsLst"
       no-gutters
@@ -31,14 +35,15 @@
         v-for="(card, index) in cardData"
         :key="index"
         cols="12"
-        md="6"
         lg="4"
       >
         <v-card
           class="apps"
           variant="flat"
+          :href="card.href"
+          target="_blank"
         >
-          <div class="apps-top">
+          <div class="apps-flex">
             <div class="thumnail">
               <v-img
                 :width="60"
@@ -71,9 +76,9 @@
               </v-chip>
             </div>
           </div>
-
+          
           <!-- 제목 -->
-          <v-card-title class="title-5">
+          <v-card-title class="title-5 pt-8">
             {{ card.title }}
           </v-card-title>
 
@@ -84,6 +89,86 @@
         </v-card>
       </v-col>
     </v-row>
+    <!--// 앱 목록  -->
+    
+    <p class="title-2 pb-10">
+      가장 많은 좋아요를 받았어요!        
+    </p>
+    <!-- 앱 목록 -->
+    <v-row
+      class="appsLst"
+      no-gutters
+    >
+      <v-col
+        v-for="(card, index) in cardData"
+        :key="index"
+        cols="12"
+        lg="4"
+      >
+        <v-card
+          class="apps"
+          variant="flat"
+          target="_blank"
+        >
+          <!-- :href="card.href" -->
+          <div class="apps-flex align-center">
+            <div class="like">
+              <v-btn
+                color="medium-emphasis"
+                prepend-icon="mdi-heart"
+                size="small"
+              >
+                999
+              </v-btn>
+            </div>
+            <!-- 배지 (최대 3개) -->
+            <div class="flags">
+              <v-chip
+                v-for="(badge, idx) in card.badges.slice(0, 3)"
+                :key="idx"
+                :color="badge.color"
+                variant="tonal"
+                density="comfortable"
+                class="flag ma-1"
+              >
+                {{ badge.text }}
+              </v-chip>
+
+              <!-- 상태 배지 (필요할 때만 표시) -->
+              <v-chip
+                class="flag"
+                density="comfortable"
+                color="primary"
+              >
+                사용중
+              </v-chip>
+            </div>
+          </div>
+
+          <!-- 제목 -->
+          <div class="apps-flex align-end pt-8">
+            <div class="context">
+              <v-card-subtitle>총무형</v-card-subtitle>
+              <v-card-title class="title-5">
+                {{ card.title }}
+              </v-card-title>
+            </div>
+            <div class="btns">
+              <v-btn
+                color="tertiary"
+                variant="outlined"
+              >
+                앱 열기
+              </v-btn>
+              <!-- <v-btn color="primary">
+                신청 대기중
+              </v-btn> -->
+            </div>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!--// 앱 목록 -->
   </v-container>
 </template>
 
@@ -101,6 +186,7 @@
 
   const cardData = [
     {
+      href: "#",
       imageSrc: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
       title: "공통총무알리미",
       description: "농협 및 축협 이용 시 필수 예약 과정으로 편리한 예약 신청 및 관리를 도와주는 편리한 농협 앱",
@@ -111,6 +197,7 @@
       ],
     },
     {
+      href: "#",
       imageSrc: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
       title: "공통총무알리미",
       description: "농협 및 축협 이용 시 필수 예약 과정으로 편리한 예약 신청 및 관리를 도와주는 편리한 농협 앱",
@@ -121,6 +208,7 @@
       ],
     },
     {
+      href: "#",
       imageSrc: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
       title: "공통총무알리미",
       description: "농협 및 축협 이용 시 필수 예약 과정으로 편리한 예약 신청 및 관리를 도와주는 편리한 농협 앱",
@@ -130,6 +218,7 @@
       ],
     },
     {
+      href: "#",
       imageSrc: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
       title: "공통총무알리미",
       description: "농협 및 축협 이용 시 필수 예약 과정으로 편리한 예약 신청 및 관리를 도와주는 편리한 농협 앱",
