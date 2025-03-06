454f1d05cd606158a38c7ce1e31d29b59d5b96c3
LAPTOP-20P7R6SQ\joo2 <ehrqor1641@gmail.com>
2025-03-06 12:03:49 +0900

    Apps 추가

---
+++ src/pages/Apps.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="fill-height"
+    style="background:#fff"
+  >
+    <p v-if="!isMobile">
+      💻 PC 화면입니다!
+    </p>
+    <p v-else>
+      📱 모바일 화면입니다!
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { inject } from 'vue';
+const isMobile = inject('isMobile');
+</script>
