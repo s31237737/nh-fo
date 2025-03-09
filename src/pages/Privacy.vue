3474004d62e343a20e55908d3fe52bc4c3b0978e
재영 <s1237737@nate.com>
2025-03-10 08:41:19 +0900

    정리

---
+++ src/pages/Privacy.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container
+    class="inner"
+  >
+    <p class="title-2 pb-10">
+      앱 전체 리스트
+    </p>
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+</script>
