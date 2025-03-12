1f82fd733d4a946f6eeeb09a9e4c0156b44ac2ff
재영 <s1237737@nate.com>
2025-03-12 09:17:31 +0900

    헤더작업중

--- src/components/GnbDrawer.vue
+++ src/components/GnbDrawer.vue
@@ -21,11 +21,53 @@
           @click="emit('update:modelValue', false)"
         />
       </v-card-title>
+      <v-card-text>
+        <v-list>
+          <template
+            v-for="(item, i) in menuItems"
+            :key="i"
+          >
+            <!-- 하위 항목이 없으면 v-list-item로 표시 -->
+            <v-list-item
+              v-if="!item.submenu"
+              :to="item.to"
+              :title="item.title"
+              active-class="is-active"
+            />
+
+            <!-- 하위 항목이 있으면 v-list-group로 표시 -->
+            <v-list-group
+              v-else
+              v-bind="item.submenuProps"
+            >
+              <template #activator="{ props }">
+                <v-list-item
+                  v-bind="props"
+                  :title="item.title"
+                />
+              </template>
+
+              <v-list-item
+                v-for="(subItem, index) in item.submenu"
+                :key="index"
+                :to="subItem.to"
+                :title="subItem.title"
+                :class="['sub-menu',{'is-active':item.isActive}]"
+              />
+            </v-list-group>
+          </template>
+        </v-list>
+      </v-card-text>
     </v-card>
   </v-navigation-drawer>
 </template>
 
 <script setup>
+import { inject, ref } from 'vue';
+
+const isMobile = inject('isMobile');
+const isTablet = inject('isTablet');
+const isDesktop = inject('isDesktop');
 
 defineProps({
   modelValue: {
@@ -35,4 +77,30 @@ defineProps({
 });
 
 const emit = defineEmits(['update:modelValue']);
+
+const menuItems = ref([
+  { title: "About",  to: "" },
+  { title: "Apps",  to: "" },
+  {
+    title: "새소식 및 도움말",
+    submenu: [
+      { title: "새소식", to: "" },
+      { title: "자주 묻는 질문", to: "" },
+      { title: "Q&A", to: "" },
+      { title: "앱가이드", to: "" }
+    ],
+    submenuProps: { value: "news" },
+  },
+  { title: "커뮤니티",  to: "" },
+  {
+    title: "마이페이지",
+    submenu: [
+      { title: "프로필", to: "" },
+      { title: "내 앱", to: "" },
+      { title: "작성한 게시물", to: "" }
+    ],
+    submenuProps: { value: "mypage" },
+  },
+]);
+
 </script>
