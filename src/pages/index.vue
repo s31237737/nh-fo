3f4114cf247c1ee21271d10fc4f75581ec5581a2
재영 <s1237737@nate.com>
2025-03-13 14:10:31 +0900

    메인작업중

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,29 +3,35 @@
     <v-container
       class="inner"
     >
-      <v-row>
-        <v-col
-          v-for="(item, index) in quickLinks"
-          :key="index"
-          :md="6"
-          :sm="12"
-        >
-          <v-card
-            class="quick-links"
-            :subtitle="item.subtitle"
-            :title="item.title"
-            :color="item.color"
-            :to="item.to"
+      <div class="user">
+        김농협님
+        안녕하세요.
+      </div>
+      <div class="content">
+        <v-row>
+          <v-col
+            v-for="(item, index) in quickLinks"
+            :key="index"
+            :md="6"
+            :sm="12"
           >
-            <template #append>
-              <v-icon
-                size="54"
-                :icon="item.appendIcon"
-              />
-            </template>
-          </v-card>
-        </v-col>
-      </v-row>
+            <v-card
+              class="quick-links"
+              :subtitle="item.subtitle"
+              :title="item.title"
+              :color="item.color"
+              :to="item.to"
+            >
+              <template #append>
+                <v-icon
+                  size="54"
+                  :icon="item.appendIcon"
+                />
+              </template>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
     </v-container>
     <Footer />
   </v-main>
