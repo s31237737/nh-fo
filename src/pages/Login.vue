3b5d38ead74a02a1a9ba14eca537e9056375ac72
주희 <ehrqor1641@gmail.com>
2025-03-17 17:42:39 +0900

    login 작업중

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -21,30 +21,41 @@
     </div>
     <div
       class="loginForm"
-    >
-      <strong class="title-4-bd">간편하게 로그인하고<br>편리한 앱들을 사용해보세요.</strong>
-      <v-btn
-        size="x-large"
-        color="yellow"
-      >
-        카카오 간편 로그인
-      </v-btn>
-
+    > 
       <div>
-        <p v-if="!isMobile">
-          다른 방법으로 시작하기
-        </p>
-        <p v-if="isMobile">
-          OR
+        <strong class="title-4-bd">간편하게 로그인하고<br>편리한 앱들을 사용해보세요.</strong>
+        <v-btn
+          size="x-large"
+          color="yellow"
+          block
+        >
+          카카오 간편 로그인
+        </v-btn>
+
+        <div class="divider">
+          <p class="text-3-bd">
+            <span>
+              {{ isMobile ? "OR" : "다른 방법으로 시작하기" }}
+            </span>
+          </p>
+        </div>
+        <v-btn
+          size="large"
+          color="info"
+        >
+          <v-icon
+            icon="custom:microsoft"
+            size="large"
+          />MS 간편로그인
+        </v-btn>
+        <p class="support-txt">
+          <v-icon
+            icon="custom:surpport"
+            size="x-small"
+          />
+          <span class="text-4">최초 비밀번호는 아이디와 동일한 개인번호입니다.</span>
         </p>
       </div>
-      <v-btn
-        size="large"
-        color="info"
-      >
-        <v-icon icon="custom:microsoft" />MS 간편로그인
-      </v-btn>
-      <p><v-icon icon="custom:surpport" />최초 비밀번호는 아이디와 동일한 개인번호입니다.</p>
     </div>
   </v-main>
 </template>
