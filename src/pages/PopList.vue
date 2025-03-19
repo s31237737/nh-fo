7b68613b4b128a4c6c25eafb2c22e50050ff7b50
주희 <ehrqor1641@gmail.com>
2025-03-19 16:32:39 +0900

    popup 작업중

--- src/pages/PopList.vue
+++ src/pages/PopList.vue
@@ -1,9 +1,56 @@
 <template>
-  <div class="inner d-flex ga-3 flex-wrap">
+  <div class="inner">
     <!-- <popup /> -->
 
     <v-row>
       <v-col cols="auto">
+        <v-btn
+          size="small"
+          color="purple"
+          @click="alert01 = true"
+        >
+          로그인 연장
+        </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn
+          size="small"
+          color="purple"
+          @click="alert02 = true"
+        >
+          페이지 나가기
+        </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn
+          size="small"
+          color="purple"
+          @click="alert03 = true"
+        >
+          앱 사용 신청
+        </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn
+          size="small"
+          color="purple"
+          @click="alert04 = true"
+        >
+          삭제 게시글
+        </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn
+          size="small"
+          color="purple"
+          @click="alert05 = true"
+        >
+          앱 사용 중지
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
         <v-btn
           size="small"
           color="purple"
@@ -12,17 +59,200 @@
           popup
         </v-btn>
       </v-col>
-      <v-col cols="auto">
-        <v-btn
-          size="small"
-          color="purple"
-          @click="dialog2 = true"
-        >
-          popup-sm(alert)
-        </v-btn>
-      </v-col>
     </v-row>
 
+    
+    <!-- popup-sm(alert) -->
+    <!-- 로그인 연장 -->
+    <v-dialog
+      v-model="alert01"
+      class="popup-sm"
+    >
+      <v-card>
+        <v-card-title>
+          <v-btn
+            icon="custom:close"
+            density="comfortable"
+            @click="alert01 = false"
+          />
+        </v-card-title>
+
+        <v-card-text>
+          <!-- dialog contents -->
+          <v-icon icon="custom:complete" />
+          <strong class="title-5-bd">로그인 연장</strong>
+          <p class="text-4">
+            로그인 연장을 원하지 않으실 경우<br>
+            자동으로 로그아웃됩니다.
+          </p>
+          <p
+            class="text-4-bd mt-2"
+          >
+            남은 시간 : 60초
+          </p>
+        <!-- // dialog contents -->
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="info"
+            size="large"
+            @click="alert01 = false"
+          >
+            취소
+          </v-btn>
+          <v-btn
+            color="primary"
+            size="large"
+          >
+            등록
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- 아이콘 -->
+    <v-dialog
+      v-model="alert02"
+      class="popup-sm"
+    >
+      <v-card>
+        <v-card-title>
+          <v-btn
+            icon="custom:close"
+            density="comfortable"
+            @click="alert02 = false"
+          />
+        </v-card-title>
+
+        <v-card-text>
+          <!-- dialog contents -->
+          <v-icon icon="custom:complete" />
+          <strong class="title-5-bd">페이지를 나가시겠습니까?</strong>
+          <p class="text-4">
+            변경사항이 저장되지 않을 수 있습니다.
+          </p>
+        <!-- // dialog contents -->
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="info"
+            size="large"
+            @click="alert02 = false"
+          >
+            취소
+          </v-btn>
+          <v-btn
+            color="primary"
+            size="large"
+          >
+            나가기
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog
+      v-model="alert03"
+      class="popup-sm"
+    >
+      <v-card>
+        <v-card-title>
+          <v-btn
+            icon="custom:close"
+            density="comfortable"
+            @click="alert03 = false"
+          />
+        </v-card-title>
+
+        <v-card-text>
+          <!-- dialog contents -->
+          <v-icon icon="custom:complete" />
+          <strong class="title-5-bd">앱 사용 신청 완료</strong>
+          <p class="text-4">
+            [앱이름] 사용 신청이 완료되었습니다.
+          </p>
+        <!-- // dialog contents -->
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="primary"
+            size="large"
+            @click="alert03 = false"
+          >
+            확인
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    
+    <v-dialog
+      v-model="alert04"
+      class="popup-sm"
+    >
+      <v-card>
+        <v-card-title>
+          <v-btn
+            icon="custom:close"
+            density="comfortable"
+            @click="alert04 = false"
+          />
+        </v-card-title>
+
+        <v-card-text>
+          <!-- dialog contents -->
+          <v-icon icon="custom:complete" />
+          <strong class="title-5-bd">관리자 삭제 게시글</strong>
+          <p class="text-4">
+            관리자에 의해 삭제된 게시글입니다.
+          </p>
+        <!-- // dialog contents -->
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="primary"
+            size="large"
+            @click="alert04 = false"
+          >
+            확인
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog
+      v-model="alert05"
+      class="popup-sm"
+    >
+      <v-card>
+        <v-card-title>
+          <v-btn
+            icon="custom:close"
+            density="comfortable"
+            @click="alert05 = false"
+          />
+        </v-card-title>
+
+        <v-card-text>
+          <!-- dialog contents -->
+          <v-icon icon="custom:complete" />
+          <strong class="title-5-bd">앱 사용중지 안내</strong>
+          <p class="text-4">
+            일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십
+          </p>
+        <!-- // dialog contents -->
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            color="primary"
+            size="large"
+            @click="alert05 = false"
+          >
+            확인
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    
 
     <!-- popup(default) -->
     <v-dialog
@@ -62,47 +292,6 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-
-    <!-- popup-sm(alert) -->
-    <v-dialog
-      v-model="dialog2"
-      class="popup-sm"
-    >
-      <v-card>
-        <v-card-title>
-          <v-btn
-            icon="custom:close"
-            density="comfortable"
-            @click="dialog2 = false"
-          />
-        </v-card-title>
-
-        <v-card-text>
-          <!-- dialog contents -->
-          <v-icon icon="custom:complete" />
-          <strong class="title-5-bd">sdfsdfd</strong>
-          <p>
-            등록하시겠습니까?
-          </p>
-        <!-- // dialog contents -->
-        </v-card-text>
-        <v-card-actions>
-          <v-btn
-            color="info"
-            size="large"
-            @click="dialog2 = false"
-          >
-            취소
-          </v-btn>
-          <v-btn
-            color="primary"
-            size="large"
-          >
-            등록
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </div>
 </template>
 
@@ -110,5 +299,10 @@
   import { ref } from 'vue';
 
   const dialog1 = ref(false);
-  const dialog2 = ref(false);
+
+  const alert01 = ref(false);
+  const alert02 = ref(false);
+  const alert03 = ref(false);
+  const alert04 = ref(false);
+  const alert05 = ref(false);
 </script>
