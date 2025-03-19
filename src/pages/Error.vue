40419a2497c92c33b17ce33ed22833c5b102389c
재영 <s1237737@nate.com>
2025-03-19 13:35:50 +0900

    에러페이지

--- src/pages/Error.vue
+++ src/pages/Error.vue
@@ -30,7 +30,7 @@ const errorMessages = computed(() => {
       return {
         title: '403 Forbidden',
         subtitle: '죄송합니다. 접근이 거부되었습니다.',
-        context: '요청하신 페이지 접근이 거부되었습니다.<br>입력하신 페이지의 주소가 정확한지 다시 한번 확인해주시기 바랍니다.'
+        context: '요청하신 페이지 접근이 거부되었습니다.\n입력하신 페이지의 주소가 정확한지 다시 한번 확인해주시기 바랍니다.'
       };
     case 'network':
       return {
@@ -45,13 +45,13 @@ const errorMessages = computed(() => {
     case '500':
       return {
         title: '500 Internal Server Error',
-        subtitle: '서버가 요청을 처리하는 중에 문제가 발생했습니다.<br>서버 설정이나 코드에 문제가 있을 수 있습니다.'
+        subtitle: '서버가 요청을 처리하는 중에 문제가 발생했습니다.\n서버 설정이나 코드에 문제가 있을 수 있습니다.'
       };
     default:
       return {
         title: '404 Not Found',
         subtitle: '죄송합니다. 현재 찾을 수 없는 페이지를 요청하셨습니다.',
-        context: '존재하지 않는 주소를 입력하셨거나,<br>요청하신 페이지의 주소가 변경 / 삭제되어 찾을 수 없습니다.'
+        context: '존재하지 않는 주소를 입력하셨거나,\n요청하신 페이지의 주소가 변경 / 삭제되어 찾을 수 없습니다.'
       };
   }
 });
