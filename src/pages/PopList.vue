55c4ea798b74a64c6195c6d4c54b7bdb7e91a5a0
주희 <ehrqor1641@gmail.com>
2025-03-20 11:07:09 +0900

    popup, input 작업중

--- src/pages/PopList.vue
+++ src/pages/PopList.vue
@@ -70,6 +70,111 @@
       </v-col>
     </v-row>
 
+    <!-- formlist -->
+    <div class="formlist">
+      <v-row>
+        <v-col>
+          <v-label for="custom-input">
+            label1-1
+          </v-label>
+          <div class="label-form">
+            <span class="text-value">text-value</span>
+          </div>
+        </v-col>
+        <v-col>
+          <v-label
+            for="custom-input"
+            class="require"
+          >
+            label1-2
+          </v-label>
+          <div class="label-form">
+            <v-switch
+              v-model="btnSwitch"
+              class="switch-flip"
+            />
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-label
+            for="custom-input"
+            class="require"
+          >
+            label2
+          </v-label>
+          <div class="label-form">
+            <v-text-field id="custom-input" />
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-label
+            for="custom-input"
+            class="require"
+          >
+            label2
+          </v-label>
+          <div class="label-form">
+            <v-select
+              v-model="select"
+              density="default"
+              :items="['전체', '타입1', '타입2']"
+            />
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-label>label3</v-label>
+          <div class="label-form">
+            <v-file-input
+              v-model="files"
+              multiple
+            >
+              <template #append-inner>
+                <v-btn
+                  color="secondary"
+                >
+                  파일첨부
+                </v-btn>
+              </template>
+            </v-file-input>
+            <!-- 첨부된 파일 목록 표시 -->
+            <div class="upload-guide">
+              <p>*첨부 가능 최대 용량은 100MB, 5개 까지 업로드 가능합니다.</p>
+            </div>
+            <ul
+              v-if="files.length"
+              class="file-list"
+            >
+              <li
+                v-for="(file, index) in files"
+                :key="index"
+              >
+                <v-icon icon="custom:file" />
+                <span>{{ file.name }}</span>
+                <v-btn
+                  density="compact"
+                  icon="custom:close"
+                  @click="removeFile(index)"
+                />
+              </li>
+            </ul>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-label>label4</v-label>
+          <div class="label-form">
+            <v-textarea />
+          </div>
+        </v-col>
+      </v-row>
+    </div>
     
     <!-- popup-sm(alert) -->
     <!-- 로그인 연장 -->
@@ -320,110 +425,6 @@
           <p class="text-4">
             업스토어 이용 중, 앱 사용 중 느낀 불편사항을 남겨주시면 관리자가 확인하여 답변드리겠습니다.
           </p>
-
-          <v-table-row>
-            <tbody>
-              <tr>
-                <th style="width: 10%">
-                  등록자
-                </th>
-                <td style="width: 23%">
-                  김농협(05300300)
-                </td>
-                <th style="width: 10%">
-                  공개여부
-                </th>
-                <td style="width: 23%">
-                  공개
-                </td>
-                <th style="width: 10%">
-                  답변상태
-                </th>
-                <td style="width: 24%">
-                  미답변
-                </td>
-              </tr>
-              <tr>
-                <th style="width: 10%">
-                  휴대전화
-                </th>
-                <td style="width: 23%">
-                  010-0000-0000
-                </td>
-                <th style="width: 10%">
-                  이메일
-                </th>
-                <td style="width: 23%">
-                  Nonghyup@gmail.com
-                </td>
-                <th style="width: 10%">
-                  작성일
-                </th>
-                <td style="width: 24%">
-                  YYYY.MM.DD.hh.mm
-                </td>
-              </tr>
-              <tr>
-                <th style="width: 10%">
-                  문의유형
-                </th>
-                <td style="width: 23%">
-                  앱 문의
-                </td>
-                <th style="width: 10%">
-                  앱
-                </th>
-                <td colspan="3">
-                  육묘대장
-                </td>
-              </tr>
-              <tr>
-                <th style="width: 10%;">
-                  제목
-                </th>
-                <td colspan="5">
-                  Q&A 질문 제목입니다.
-                </td>
-              </tr>
-              <tr>
-                <th style="width: 10%;">
-                  내용
-                </th>
-                <td colspan="5">
-                  <p>
-                    안녕하세요.<br>
-                    앱 사용할때 궁금한점이 있어 문의드립니다.<br>
-                    Q&A 내용은 여기에 적으면 되나요?<br>
-                    답변 부탁드립니다.<br>
-                  </p>
-                </td>
-              </tr>
-              <tr>
-                <th style="width: 10%;">
-                  첨부파일
-                </th>
-                <td colspan="5">
-                  <ul
-                    class="file-list"
-                  >
-                    <li
-                      v-for="(file, index) in files"
-                      :key="index"
-                    >
-                      <v-icon icon="custom:file" />
-                      <span>{{ file.name }}</span>
-                      <em>{{ file.size }}</em>
-                      <v-btn
-                        class="icon-md"
-                        icon="custom:download"
-                        @click="downloadFile(file)"
-                      />
-                    </li>
-                  </ul>
-                </td>
-              </tr>
-            </tbody>
-          </v-table-row>
           <!--// dialog contents -->
         </v-card-text>
         <v-card-actions>
@@ -448,6 +449,13 @@
 
 <script setup>
   import { ref } from 'vue';
+
+  const btnSwitch = ref(true);
+  const select = ref("전체");
+  const files = ref([]);
+  const removeFile = (index) => {
+    files.value.splice(index, 1);
+  };
 
   const dialog1 = ref(false);
   const dialog2 = ref(false);
