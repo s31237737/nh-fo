440a7bb09cb6d70479ace2d6453f03f7b545b7a2
재영 <s1237737@nate.com>
2025-03-21 15:50:03 +0900

    팝업

--- src/pages/Guide.vue
+++ src/pages/Guide.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="w-75 ma-auto mt-5 mb-5">
+    <Pagination
+      :total-items="dataTableItem.length"
+      :items-per-page="itemsPerPage"
+      @paging="page = $event.page"
+    />
     <!-- icons -->
     <div class="d-flex flex-wrap ga-2 bg-info">
       <div class="icon-item">
@@ -1378,6 +1383,112 @@ const likes = ref(0);
 const incrementLikes = () => {
   likes.value++;
 };
+
+
+
+// 데이터 테이블
+const page = ref(1);
+const itemsPerPage = ref(10);
+
+const dataTableItem = ref([
+  {
+    id: 1,
+    appCode: "APP001",
+    appName: "테스트 앱 1",
+    appType: "앱",
+    status: "노출",
+    answer: "답변완료",
+    registrant: "홍길동",
+    createdDate: "YYYY.MM.DD",
+    updatedDate: "YYYY.MM.DD",
+    lastModifiedDate: "YYYY.MM.DD",
+  },
+  {
+    id: 2,
+    appCode: "APP002",
+    appName: "일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십",
+    appType: "앱",
+    status: "미노출",
+    answer: "미답변",
+    registrant: "김철수",
+    createdDate: "YYYY.MM.DD",
+    updatedDate: "YYYY.MM.DD",
+    lastModifiedDate: "2025.02.16",
+  },
+  {
+    id: 3,
+    appCode: "APP003",
+    appName: "앱3",
+    appType: "앱",
+    status: "노출",
+    answer: "답변완료",
+    registrant: "이민수",
+    createdDate: "YYYY.MM.DD",
+    updatedDate: "2025.02.03",
+    lastModifiedDate: "2025.02.10",
+  },
+  {
+    id: 4,
+    appCode: "APP004",
+    appName: "앱4",
+    appType: "앱",
+    status: "미노출",
+    answer: "미답변",
+    registrant: "박지은",
+    createdDate: "YYYY.MM.DD",
+    updatedDate: "2025.02.07",
+    lastModifiedDate: "2025.02.12",
+  },
+  {
+    id: 5,
+    appCode: "APP005",
+    appName: "앱5",
+    appType: "앱",
+    status: "노출",
+    answer: "답변완료",
+    registrant: "최영희",
+    createdDate: "YYYY.MM.DD",
+    updatedDate: "YYYY.MM.DD",
+    lastModifiedDate: "2025.02.14",
+  },
+  {
+    id: 6,
+    appCode: "APP006",
+    appName: "앱6",
+    appType: "앱",
+    status: "미노출",
+    answer: "미답변",
+    registrant: "정호준",
+    createdDate: "2025.01.25",
+    updatedDate: "2025.02.09",
+    lastModifiedDate: "2025.02.13",
+  },
+  {
+    id: 7,
+    appCode: "APP007",
+    appName: "앱7",
+    appType: "앱",
+    status: "노출",
+    answer: "답변완료",
+    registrant: "홍길동",
+    createdDate: "YYYY.MM.DD",
+    updatedDate: "2025.02.10",
+    lastModifiedDate: "2025.02.18",
+  },
+  {
+    id: 8,
+    appCode: "APP008",
+    appName: "앱8",
+    appType: "앱",
+    status: "미노출",
+    answer: "미답변",
+    registrant: "김철수",
+    createdDate: "2025.02.02",
+    updatedDate: "2025.02.11",
+    lastModifiedDate: "2025.02.17",
+  },
+]);
+
 </script>
 <style scoped>
 .icon-item {
