656cf96d7bcbffb03cdbdb32c835a9db4047f819
재영 <s1237737@nate.com>
2025-03-24 10:40:44 +0900

    새소식 작업중

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,55 +1,8 @@
 <template>
-  <div>
-    <!-- 다른 버튼으로 페이지네이션 제어 -->
-    <div class="d-flex justify-center py-4">
-      <!-- 페이지 네비게이션 버튼들 -->
-      <v-btn
-        v-for="(color, i) in colors"
-        :key="i"
-        :color="model === i ? 'primary': 'success'"
-        @click="model = i"
-      >
-        {{ i + 1 }}
-      </v-btn>
-    </div>
-
-    <!-- v-carousel 컴포넌트 -->
-    <v-carousel
-      v-model="model"
-      hide-delimiters
-      cycle
-    >
-      <v-carousel-item
-        v-for="(color, i) in colors"
-        :key="color"
-        :value="i"
-      >
-        <v-sheet
-          :color="color"
-          height="100%"
-          tile
-        >
-          <div class="d-flex fill-height justify-center align-center">
-            <div class="text-h2">
-              Slide {{ i + 1 }}
-            </div>
-          </div>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel>
-  </div>
+  About
 </template>
 
 <script setup>
   import { ref } from 'vue'
 
-  const colors = [
-    'primary',
-    'secondary',
-    'yellow darken-2',
-    'red',
-    'orange',
-  ]
-
-  const model = ref(0)
 </script>
---
+++ src/pages/News.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-sheet height="370">
+    <v-container class="inner">
+      <v-card>
+        <v-card-item>
+          <v-card-subtitle>
+            새소식 및 도움말
+          </v-card-subtitle>
+          <v-card-title>
+            안녕하세요,<br>무엇을 도와드릴까요?
+          </v-card-title>
+          <v-card-text>
+            ttt
+          </v-card-text>
+          <template #append>
+            <v-img
+            width="300"
+              src="@/assets/images/img_news_banner.png"
+            />
+          </template>
+        </v-card-item>
+      </v-card>
+    </v-container>
+  </v-sheet>
+  <v-container class="inner">
+    <p>sssss</p>
+  </v-container>
+</template>
+
+<script setup>
+</script>
