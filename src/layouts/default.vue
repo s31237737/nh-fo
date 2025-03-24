caf825e882d4c5f8aaa8a0de6ccd449906150a3c
주희 <ehrqor1641@gmail.com>
2025-03-24 13:43:19 +0900

    불편신고 버튼 작업중

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,51 @@
 <template>
   <Header />
-  <v-main>
+  <v-main v-scroll="onScroll">
     <router-view />
-    <Footer />
+    <v-btn
+      size="x-large"
+      rounded="pill"
+      class="btn-floating"
+      :class="{ 'btn-bottom': nearFooter }"
+      @mouseover="hover = true"
+      @mouseleave="hover = false"
+    >
+      <v-icon
+        icon="custom:report"
+        size="x-large"
+      />
+      <p class="btn-floating-text">
+        <span>
+          불편신고
+        </span>
+        <span class="hover">
+          사용 중 불편사항이 있으신가요?
+        </span>
+      </p>
+    </v-btn>
+    <Footer ref="footerRef" />
   </v-main>
 </template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const hover = ref(false);
+const nearFooter = ref(false);
+const footerRef = ref(null);//footer 컴포넌트 참조하여 푸터의 위치를 가져옴
+
+const onScroll = () => {
+  if (!footerRef.value) return;
+
+  const footerTop = footerRef.value.$el.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  nearFooter.value = footerTop < windowHeight;
+};
+
+// onMounted(() => window.addEventListener("scroll", onScroll));
+// onUnmounted(() => window.removeEventListener("scroll", onScroll));
+</script>
+
+<style scoped>
+</style>
