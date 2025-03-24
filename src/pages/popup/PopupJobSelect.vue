a70df65bda3b4af99c9155541c01a66eea28b7db
주희 <ehrqor1641@gmail.com>
2025-03-24 17:10:35 +0900

    불편신고 팝업 작업중

---
+++ src/pages/popup/PopupJobSelect.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-dialog
+    :model-value="modelValue"
+    class="popup"
+    @update:model-value="emit('update:modelValue', $event)"
+  >
+    <v-card>
+      <v-card-title>
+        <p>직무선택</p>
+        <v-btn
+          icon="custom:close"
+          density="comfortable"
+          @click="emit('update:modelValue', false)"
+        />
+      </v-card-title>
+      <v-card-text>
+        <!-- dialog contents -->
+        
+        <!--// dialog contents -->
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          color="info"
+          size="large"
+          @click="emit('update:modelValue', false)"
+        >
+          초기화
+        </v-btn>
+        <v-btn
+          color="primary"
+          size="large"
+        >
+          선택완료
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+  //import { ref } from 'vue';
+  defineProps({
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+  });
+  const emit = defineEmits(['update:modelValue']);
+</script>
