c31737d2801caaa1c864e9c4f90b2dc2baa6381e
주희 <ehrqor1641@gmail.com>
2025-03-24 16:31:08 +0900

    footer로 버튼 테스트

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,31 @@
 <template>
-  <v-footer :height="!isMobile ? 100 : 140">
+  <v-footer
+    ref="footerRef"
+    :height="!isMobile ? 100 : 140"
+  >
     <div class="inner">
+      <v-btn
+        size="x-large"
+        rounded="pill"
+        class="btn-floating"
+        :class="{ 'btn-bottom': nearFooter }"
+        :ripple="false"
+        @mouseover="hover = true"
+        @mouseleave="hover = false"
+      >
+        <v-icon
+          icon="custom:report"
+          size="x-large"
+        />
+        <p class="btn-floating-text">
+          <span>
+            불편신고
+          </span>
+          <span class="hover">
+            사용 중 불편사항이 있으신가요?
+          </span>
+        </p>
+      </v-btn>
       <router-link to="/">
         <v-img
           :width="!isMobile ? 180 : 144"
@@ -22,7 +47,28 @@
 </template>
 
 <script setup>
-import { inject } from 'vue';
+import { ref, inject, onMounted, onUnmounted } from 'vue';
 
 const isMobile = inject('isMobile');
+const hover = ref(false);
+
+const nearFooter = ref(false);
+const footerRef = ref(null);
+
+const footerOffset = () => {
+  if (footerRef.value) {
+    const footerRect = footerRef.value.$el.getBoundingClientRect();
+    const windowHeight = window.innerHeight-40;
+    nearFooter.value = footerRect.top <= windowHeight;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', footerOffset);
+  footerOffset();
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', footerOffset);
+});
 </script>
