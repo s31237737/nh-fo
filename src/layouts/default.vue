26c13c1a573080be7d91f463911e283b5e12d160
주희 <ehrqor1641@gmail.com>
2025-03-24 15:48:00 +0900

    불편신고 버튼 작업중

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,4 +1,4 @@
-<template v-scroll="onScroll">
+<template>
   <Header />
   <v-main>
     <router-view />
@@ -29,16 +29,17 @@
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
+//
 
 const hover = ref(false);
 const nearFooter = ref(false);
-const footerRef = ref(null);//footer 컴포넌트 참조하여 푸터의 위치를 가져옴
+const footerRef = ref(true);//footer 컴포넌트 참조하여 푸터의 위치를 가져옴
 
 const onScroll = () => {
   if (!footerRef.value) return;
 
-  const footerTop = footerRef.value.$el.getBoundingClientRect().top;
-  const windowHeight = window.innerHeight;
+  const footerTop = footerRef.value.$el.getBoundingClientRect().top;//footer 컴포넌트 참조하여 푸터의 위치를 가져옴
+  const windowHeight = window.outerHeight;
 
   nearFooter.value = footerTop < windowHeight;
 };
@@ -46,6 +47,3 @@ const onScroll = () => {
 onMounted(() => window.addEventListener("scroll", onScroll));
 onUnmounted(() => window.removeEventListener("scroll", onScroll));
 </script>
-
-<style scoped>
-</style>
