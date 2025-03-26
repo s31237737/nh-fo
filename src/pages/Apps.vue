e36027fdd71050f8b12c812aa2153fa43dd9d419
주희 <ehrqor1641@gmail.com>
2025-03-26 17:16:29 +0900

    apps 작업중(poplist 링크삭제)

--- src/pages/Apps.vue
+++ src/pages/Apps.vue
@@ -615,7 +615,7 @@ const autoplay = ref(true);
 const alert = ref(false); //"앱 열기" 팝업
 const cardData = ref([
   {
-    link: "PopList",
+    link: "AppsDetail",
     imageSrc: "@temp_img_app_icon03.png",
     title: "1공통총무알리미",
     description: "1농협 및 축협 이용 시 필수 예약 과정으로 편리한 예약 신청 및 관리를 도와주는 편리한 농협 앱",
