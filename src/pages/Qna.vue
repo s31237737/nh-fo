b9434ced8d00bd20f86e2b4cb5f5cefbad867e8c
재영 <s1237737@nate.com>
2025-03-27 15:13:29 +0900

    테스트

--- src/pages/Qna.vue
+++ src/pages/Qna.vue
@@ -1,11 +1,12 @@
 <template>
-  <v-container>
+  <v-container height="200vh">
     <v-select
       ref="selectRef"
       v-model="selectedItem"
       :items="items"
       label="옵션 선택"
-      menu-icon=""
+      :menu="isMenuOpen"
+      @update:menu="isMenuOpen = $event"
     />
   </v-container>
 </template>
@@ -16,10 +17,11 @@ import { ref, onMounted, onUnmounted } from 'vue';
 const selectedItem = ref(null);
 const items = ref(['옵션 1', '옵션 2', '옵션 3']);
 const selectRef = ref(null);
+const isMenuOpen = ref(false);
 
 const closeSelectOnScroll = () => {
-  if (selectRef.value) {
-    selectRef.value.isMenuActive = false;
+  if (isMenuOpen.value) {
+    isMenuOpen.value = false; // 셀렉트 닫기
   }
 };
 
