1693ffbae2dacea063cc650b2b6b82169643c580
재영 <s1237737@nate.com>
2025-03-28 17:43:14 +0900

    새소식 작업중

--- src/pages/QnaWrite.vue
+++ src/pages/QnaWrite.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="inner">
     <div class="tit-wrap column">
-      <strong class="title-4-bd">[공지] NH총무알리비 서비스 앱 개선 공지 (24.6.20(목) 18:00~24:00)</strong>
+      <strong class="title-4-bd">Q&amp;A</strong>
       <p>문의하시기 전 자주하는 질문을 통해 문제 해결방법을 확인하실 수 있습니다.</p>
     </div>
     <div class="form-sheet">
