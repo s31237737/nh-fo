6fa9d3d9def8d579de0b4a157911dc1ff6e011e7
재영 <s1237737@nate.com>
2025-04-07 10:08:35 +0900

    마이페이지

--- src/pages/MyPage/Profile.vue
+++ src/pages/MyPage/Profile.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-    <p>y</p>
-  </div>
+  <v-row no-gutters>
+    <v-col
+      cols="12"
+      lg="3"
+      class="flex-none"
+    >
+      <v-sheet class="pa-4">
+        첫 번째 카드
+      </v-sheet>
+    </v-col>
+
+    <v-col
+      cols="12"
+      lg="9"
+    >
+      <v-sheet class="pa-4">
+        두 번째 카드
+      </v-sheet>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup>
